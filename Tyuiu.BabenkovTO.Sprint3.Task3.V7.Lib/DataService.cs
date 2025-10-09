598d00d8ee4321e1302bb072e5f676e17adc3200
BabenkovTrofim <babenkovt@bk.ru>
2025-10-09 13:19:15 +0500

    Task3

---
+++ Tyuiu.BabenkovTO.Sprint3.Task3.V7.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BabenkovTO.Sprint3.Task3.V7.Lib
+{
+    public class DataService : ISprint3Task3V7
+    {
+        public string ReplaceCharOnNum(string value, char replaceable, char replacement)
+        {
+            foreach (char i in value)
+            {
+                if (i == replaceable)
+                    value = value.Replace(i, replacement);
+            }
+            return value;
+        }
+    }
+}
---
+++ Tyuiu.BabenkovTO.Sprint3.Task3.V7/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.BabenkovTO.Sprint3.Task3.V7.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #3 | Выполнил: Бабенков Т. О. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнил: Бабенков Трофим Олегович | ИИПб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Используя цикл foreach заменить буквы g на цифру 4 в строке:            *");
+        Console.WriteLine("* gft hggt ntg                                                            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        string value = "gft hggt ntg";
+        char replaceable = 'g';
+        char replacement = '4';
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        string res = ds.ReplaceCharOnNum(value, replaceable, replacement);
+        Console.WriteLine($"Результат: {res}");
+    }
+}
