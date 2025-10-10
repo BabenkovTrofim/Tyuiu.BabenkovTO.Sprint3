ac2e97ef87b91237b8b2af70539066c5584b11a4
BabenkovTrofim <babenkovt@bk.ru>
2025-10-10 21:18:37 +0500

    Task4

---
+++ Tyuiu.BabenkovTO.Sprint3.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BabenkovTO.Sprint3.Task4.V5.Lib;
+namespace Tyuiu.BabenkovTO.Sprint3.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int startValue = -5;
+            int stopValue = 5;
+            DataService ds = new DataService();
+            double wait = -0.858;
+            Assert.AreEqual(wait, ds.Calculate(startValue, stopValue));
+        }
+    }
+}
---
+++ Tyuiu.BabenkovTO.Sprint3.Task4.V5/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.BabenkovTO.Sprint3.Task4.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #3 | Выполнил: Бабенков Т. О. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Использование операторов continue и break в циклах                      *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Выполнил: Бабенков Трофим Олегович | ИИПб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+        Console.WriteLine("* функции y=(cos(x)-sin(x))/x. При х = 0 прервать цикл.                   *");
+        Console.WriteLine("* Полученные значения суммировать.                                        *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        var res = ds.Calculate(startValue, stopValue);
+        Console.WriteLine($"Ответ = {res}");
+    }
+}
