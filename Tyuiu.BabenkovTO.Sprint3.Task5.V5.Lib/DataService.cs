95994c581a2602a604ec90d90ed92b75199c4e46
BabenkovTrofim <babenkovt@bk.ru>
2025-10-15 23:12:57 +0500

    Task5

---
+++ Tyuiu.BabenkovTO.Sprint3.Task5.V5.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BabenkovTO.Sprint3.Task5.V5.Lib
+{
+    public class DataService : ISprint3Task5V5
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double x1 = x;
+            double result = 0;
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                double res1 = 0;
+                for(double j = startValue2; j <= stopValue2; j++)
+                {
+                    res1 += x1 / (Math.Sin(j));
+                }
+                result += res1;
+            }
+            return Math.Round(result, 3);
+        }
+    }
+}
---
+++ Tyuiu.BabenkovTO.Sprint3.Task5.V5/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.BabenkovTO.Sprint3.Task5.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #3 | Выполнил: Бабенков Т. О. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Использование операторов continue и break в циклах                      *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Выполнил: Бабенков Трофим Олегович | ИИПб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя вложенные циклы, которая вычисляет         *");
+        Console.WriteLine("* значение ряда по формуле                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int iStart = 1, iStop = 3, kStart = 1, kStop = 10, x = 5;
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        var res = ds.GetSumSumSeries(x, iStart, kStart, iStop, kStop);
+        Console.WriteLine($"Ответ: {res}");
+    }
+}
